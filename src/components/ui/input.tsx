cb854f02fa6f6d00e282b79aff11c258590c91af
Safa Aamir <147985686+isafabaig@users.noreply.github.com>
2025-05-10 17:03:27 +0500

    Fix repository setup

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,5 @@
 import { InputHTMLAttributes } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export default function Input({ ...props }: InputProps) {
+export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return <input className="w-full border p-2 rounded" {...props} />;
 }
